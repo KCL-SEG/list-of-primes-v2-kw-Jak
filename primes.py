e803e977a406f471f48f95a17e7e184768edc58a
Ka (Ka Wai) Lau <kwlau107@gmail.com>
2023-11-15 01:13:11 +0000

    Add code for generating prime numbers in primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,15 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+
     list = []
+    current_num = 2
+    while len(list) < number_of_primes:
+        if all(current_num % prime != 0 for prime in list):
+            list.append(current_num)
+        current_num += 1
     return list
